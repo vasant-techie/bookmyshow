73517fe75ad46ca3b5463c20119f6456f390bda1
Vasant <vasant.techie@gmail.com>
2023-10-11 00:26:13 +0530

    1) removed redundant entry of bms.authorities table and 2) refactored the ddl scripts

--- bms-consumer-api/src/main/resources/schema.sql
+++ bms-consumer-api/src/main/resources/schema.sql
@@ -1,11 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS bms;
 
-CREATE TABLE IF NOT EXISTS bms.authorities (
-   id INT NOT NULL AUTO_INCREMENT,
-   username VARCHAR(45) NOT NULL,
-   authority VARCHAR(45) NOT NULL,
-   PRIMARY KEY (id));
-
 CREATE TABLE IF NOT EXISTS bms.users (
   userid INT NOT NULL AUTO_INCREMENT,
   username VARCHAR(45) NOT NULL,
@@ -43,18 +37,18 @@ CREATE TABLE IF NOT EXISTS bms.theatres (
 
 CREATE TABLE IF NOT EXISTS bms.shows (
   showid INT NOT NULL AUTO_INCREMENT,
-  theatreid INT NOT NULL REFERENCES theatres(theatreid) ON DELETE CASCADE,
-  movieid INT NOT NULL REFERENCES  movies(movieid) ON DELETE CASCADE,
+  theatreid INT NOT NULL REFERENCES bms.theatres(theatreid) ON DELETE CASCADE,
+  movieid INT NOT NULL REFERENCES  bms.movies(movieid) ON DELETE CASCADE,
   date DATE NOT NULL,
   time TIME NOT NULL,
   PRIMARY KEY (showid));
 
 CREATE TABLE IF NOT EXISTS bms.bookings (
   bookingid INT NOT NULL AUTO_INCREMENT,
-  theatreid INT NOT NULL REFERENCES theatres(theatreid) ON DELETE CASCADE,
-  movieid INT NOT NULL REFERENCES  movies(movieid) ON DELETE CASCADE,
-  bookedbyuserid INT NOT NULL REFERENCES users(userid) ON DELETE CASCADE,
-  showid INT NOT NULL REFERENCES  shows(showid) ON DELETE CASCADE,
+  theatreid INT NOT NULL REFERENCES bms.theatres(theatreid) ON DELETE CASCADE,
+  movieid INT NOT NULL REFERENCES  bms.movies(movieid) ON DELETE CASCADE,
+  bookedbyuserid INT NOT NULL REFERENCES bms.users(userid) ON DELETE CASCADE,
+  showid INT NOT NULL REFERENCES  bms.shows(showid) ON DELETE CASCADE,
   seats INT NOT NULL,
   bookingdate DATE NOT NULL,
   bookingtime TIME NOT NULL,

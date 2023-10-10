3fadbddb85bc536e1be5013abf3fc1fd2b3fa357
Vasant <vasant.techie@gmail.com>
2023-10-10 23:46:54 +0530

    1) Enabled H2 Console 2) Introduced Secuity Handler class to permit uri's /h2-console and /actuator and 3) externalized h2 database configuration to application properties file

--- bms-consumer-api/src/main/java/com/bookmyshow/consumer/api/ConsumerApiApp.java
+++ bms-consumer-api/src/main/java/com/bookmyshow/consumer/api/ConsumerApiApp.java
@@ -1,12 +1,10 @@
 package com.bookmyshow.consumer.api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
+@SpringBootApplication
 @EnableJpaRepositories
 public class ConsumerApiApp {
     public static void main(String[] args) {
---
+++ bms-consumer-api/src/main/java/com/bookmyshow/consumer/api/security/BMSSecurityHandler.java
@@ -0,0 +1,37 @@
+package com.bookmyshow.consumer.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+@EnableWebSecurity
+public class BMSSecurityHandler {
+
+    @Bean
+    MvcRequestMatcher.Builder matcher(HandlerMappingIntrospector handlerMappingIntrospector) {
+        return new MvcRequestMatcher.Builder(handlerMappingIntrospector);
+    }
+
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http, MvcRequestMatcher.Builder mvcMatcher) throws Exception {
+
+        http.authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(mvcMatcher.servletPath("/actuator").pattern("**")).permitAll() // just an example of an endpoint
+                        .requestMatchers(mvcMatcher.servletPath("/h2-console").pattern("**")).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        return http.build();
+    }
+
+}
